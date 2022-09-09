5ac246dd02cc7bd764a1451529c764d00783e4dc
Adams Uthman <uthmanadams22@gmail.com>
2022-09-09 14:25:52 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabets in uppercase and lowercase
+ *Return:0 successs
+ */
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z') 
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+
+	return (0);
+}
