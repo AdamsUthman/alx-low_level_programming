f2d80edd305705670004e6b1075218ee79b65d4c
Adams Uthman <uthmanadams22@gmail.com>
2022-09-09 15:43:26 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - prints 00 to 99
+ *Return:0 Success
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			putchar(tens);
+			putchar(ones);
+			if (!(tens == '9' && ones == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
