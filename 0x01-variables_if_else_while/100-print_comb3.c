b9c8d0aea4ddb8f60a320756ef5d0133e25fbe7d
Adams Uthman <uthmanadams22@gmail.com>
2022-09-09 16:57:05 -0700

    correcting mistakes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
 
+/**
+ *main - Print numbers from 00 - 99
+ *Return:0 Success
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (i = (n + 1); i <= '9'; i++)
+		{
+			putchar(n);
+			putchar(i);
+
+			if (n != '8' || i != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
