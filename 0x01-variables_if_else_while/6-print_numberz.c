3319c9d5ca02843955a385909eea5daf45be80b0
Adams Uthman <uthmanadams22@gmail.com>
2022-09-09 14:55:50 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - printing numbers
+ *Return:0 Success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= '9')
+	{
+		putchar(n);
+	}
+	putchat('\n');
+
+	return (0);
+}
