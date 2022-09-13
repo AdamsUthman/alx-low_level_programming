3111f9875c138e2c460cf4ff6244846f2109d7b8
Adams Uthman <uthmanadams22@gmail.com>
2022-09-13 16:49:23 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table
+ */
+void times__table(void)
+{
+	int row;
+	int column;
+	int product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = (row * column);
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+		}
+		_putchar('\n');
+	}
+}
