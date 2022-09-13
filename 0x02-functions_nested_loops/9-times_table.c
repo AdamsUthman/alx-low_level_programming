2b83352df50132c028ac8a316a517a463cac0fc7
Adams Uthman <uthmanadams22@gmail.com>
2022-09-13 16:57:00 -0700

    initial commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,6 +25,12 @@ void times__table(void)
 				_putchar(' ');
 				_putchar('0' + product);
 			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
 		}
 		_putchar('\n');
 	}
