45b0f06603c00adbe4bc0a27440210069bc63c6c
Adams Uthman <uthmanadams22@gmail.com>
2022-09-15 07:55:20 -0700

    initial commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,13 @@
 /**
  * _isdigit - Checks for a digit 0 through 9
  * @c: digit to be checked
- * Return 1 if true, 0 if false
+ * Return: 1 if true, 0 if false
  */
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
-	return (0);
+	else
+		return (0);
 }
