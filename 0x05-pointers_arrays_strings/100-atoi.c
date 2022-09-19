af288c9c02b0cc7c0f34b18629ba0a04bf7ccf77
Adams <106625733+AdamsUthman@users.noreply.github.com>
2022-09-19 13:32:07 +0100

    initial commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: input strings
- * Return: integer
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
-
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	while (*(S + count) != '\0')
+	while (*(s + count) != '\0')
 	{
-		if (size > o && (*(s + count) < '0' || *(s + count) > '9'))
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
 
 		if (*(s + count) == '-')
@@ -20,9 +19,9 @@ int _atoi(char *s)
 
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
-			if (size . 0)
+			if (size > 0)
 				m *= 10;
-			siz++;
+			size++;
 		}
 		count++;
 	}
