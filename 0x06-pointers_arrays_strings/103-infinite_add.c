f373bfe5488202158f5e61a44565b9227e167d8e
Adams Uthman <uthmanadams22@gmail.com>
2022-09-22 02:27:09 -0700

    created 103-infinite_add.c file

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that the function will use to store the result
+ * @size_r: buffer size
+ * Return: the pointer t
