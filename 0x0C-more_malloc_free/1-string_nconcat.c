81d25637f5e3323e8f117ae1b37c48f9af5a849a
Adams Uthman <uthmanadams22@gmail.com>
2022-10-07 03:12:36 -0700

    initial commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * string_noncat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
+ */
+
+char *string_noncat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	uns
