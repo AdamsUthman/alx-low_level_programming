48decf9d7f886d2a3ca9cabf9c17da405f72fd2d
Adams Uthman <uthmanadams22@gmail.com>
2022-10-21 08:55:57 -0700

    correcting mistakes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@
 void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must alllow");
-	printf(",\nI bore my house upon my back!\n);
+	printf(",\nI bore my house upon my back!\n");
 }
