9c7fc0dd26b9f0748081c74cce38d688ccf78123
Adams <106625733+AdamsUthman@users.noreply.github.com>
2022-10-27 13:35:53 +0100

    Correcting files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * binary_to_int - converts a binary to an unsigned int
- * @b: binary
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: unsigned int
+ * Return: unsigned int.
  */
-
-unsigned int binary_to_int(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
 	int len, base_two;
