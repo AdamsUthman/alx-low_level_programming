f2f7e7336733c652e54861b41acd6c306a0b1d8d
Adams <106625733+AdamsUthman@users.noreply.github.com>
2023-01-10 15:22:17 +0100

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * key_index - function that generatesa distributed index
+ * key_index - function that generates a distributed index
  * according to a given hash
  *
  * @key: key passed
@@ -10,6 +10,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	if (key == Null || size == 0)
+	if (key == NULL || size == 0)
 		return (0);
+	return (hash_djb2(key) % size);
 }
